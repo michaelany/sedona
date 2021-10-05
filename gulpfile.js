259d8062ee7d9507d1855277623731c7a21cfc02
Michael Any <michael.any.dev@gmail.com>
2021-10-05 17:50:42 +0300

    base changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,105 +1,99 @@
-const fileinclude = require('gulp-file-include');
+const project_folder = "dist";
+const source_folder = "src";
 
-let project_folder = "dist";
-let source_folder = "src";
+const path = {
+  build: {
+    html: project_folder + "/",
+    css: project_folder + "/css/",
+    js: project_folder + "/js/",
+    img: project_folder + "/img/",
+    fonts: project_folder + "/fonts/",
+  },
 
-let path = {
-    build: {
-        html: project_folder + "/",
-        css: project_folder + "/css/",
-        js: project_folder + "/js/",
-        img: project_folder + "/img/",
-        fonts: project_folder + "/fonts/",
-    },
+  src: {
+    html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
+    css: source_folder + "/styles/style.scss",
+    js: source_folder + "/js/script.js",
+    img: source_folder + "/img/**/*.{jpg, png, svg, gif, ico, webp}",
+    fonts: source_folder + "/fonts/*.ttf",
+  },
 
-    src: {
-        html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
-        css: source_folder + "/scss/style.scss",
-        js: source_folder + "/js/script.js",
-        img: source_folder + "/img/**/*.{jpg, png, svg, gif, ico, webp}",
-        fonts: source_folder + "/fonts/*.ttf",
-    },
+  watch: {
+    html: source_folder + "/**/*.html",
+    css: source_folder + "/styles/**/*.scss",
+    js: source_folder + "/js/**/*.js",
+    img: source_folder + "/img/**/*.{jpg, png, svg, gif, ico, webp}",
+  },
 
-    watch: {
-        html: source_folder + "/**/*.html",
-        css: source_folder + "/scss/**/*.scss",
-        js: source_folder + "/js/**/*.js",
-        img: source_folder + "/img/**/*.{jpg, png, svg, gif, ico, webp}",
-    },
+  clean: "./" + project_folder + "/",
+};
 
-    clean: "./" + project_folder + "/"
-}
-
-let { src, dest } = require('gulp'),
-    gulp = require('gulp'),
-    browsersync = require('browser-sync').create(),
-    fileincl = require('gulp-file-include'),
-    del = require('del'),
-    scss = require('gulp-sass'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify-es').default;
+const { src, dest } = require("gulp"),
+  gulp = require("gulp"),
+  browsersync = require("browser-sync").create(),
+  fileincl = require("gulp-file-include"),
+  del = require("del"),
+  scss = require("gulp-sass")(require("sass")),
+  rename = require("gulp-rename"),
+  uglify = require("gulp-uglify-es").default;
 
 function browserSync(params) {
-    browsersync.init({
-        server: {
-            baseDir: "./" + project_folder + "/"
-        },
-        port: 3000,
-        notify: false
-    })
+  browsersync.init({
+    server: {
+      baseDir: "./" + project_folder + "/",
+    },
+    port: 3000,
+    notify: false,
+  });
 }
 
 function html() {
-    return src(path.src.html)
-        .pipe(fileincl())
-        .pipe(dest(path.build.html))
-        .pipe(browsersync.stream())
+  return src(path.src.html)
+    .pipe(fileincl())
+    .pipe(dest(path.build.html))
+    .pipe(browsersync.stream());
 }
 
 function css() {
-    return src(path.src.css)
-        .pipe(
-            scss({
-                outputStyle: "expanded"
-            })
-        )
-        .pipe(dest(path.build.css))
-        .pipe(browsersync.stream())
+  return src(path.src.css)
+    .pipe(
+      scss({
+        outputStyle: "expanded",
+      })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream());
 }
 
 function js() {
-    return src(path.src.js)
-        .pipe(fileincl())
-        .pipe(dest(path.build.js))
-        .pipe(
-            uglify()
-        )
-        .pipe(
-            rename({
-                extname: ".min.js"
-            })
-        )
-        .pipe(dest(path.build.js))
-        .pipe(browsersync.stream())
+  return src(path.src.js)
+    .pipe(fileincl())
+    .pipe(dest(path.build.js))
+    .pipe(uglify())
+    .pipe(
+      rename({
+        extname: ".min.js",
+      })
+    )
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream());
 }
 
 function watchFiles(params) {
-    gulp.watch([path.watch.html], html);
-    gulp.watch([path.watch.js], js);
+  gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch.js], js);
 }
 
 function clean(params) {
-    return del(path.clean);
+  return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html));
-let watch = gulp.parallel(build, watchFiles, browserSync);
+const build = gulp.series(clean, gulp.parallel(js, css, html));
+const watch = gulp.parallel(build, watchFiles, browserSync);
 
 exports.js = js;
 exports.css = css;
 exports.html = html;
 exports.build = build;
 exports.watch = watch;
-exports.default = watch;;
-
-
+exports.default = watch;
